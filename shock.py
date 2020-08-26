5eb31110c5bfad6efabee997ad1834ecd8bab50e
Haris Khan <haris294@gmail.com>
2020-08-26 23:20:58 +0530

    Delete shock.py

--- shock.py
+++
@@ -1,9 +0,0 @@
-import webbrowser
-import time
-import random
-
-while True:
-    sites = random.choice(['google.com','youtube.com','amazon.com'])
-    visit = "http://{}".format(sites)
-    webbrowser.open(visit)
-    #seconds = random.randrange()
